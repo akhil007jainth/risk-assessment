f3b39a6e3015226c449810e6fc06a072b3bd7e42
Shikharg02 <shikhar.gupta@surepass.io>
2024-09-13 19:48:42 +0530

    added date in model

--- web/models/main.py
+++ web/models/main.py
@@ -1,6 +1,9 @@
-from mongoengine import Document, StringField
+from datetime import datetime
+
+from mongoengine import Document, StringField, DateTimeField
 
 
 class User(Document):
     username = StringField(required=True, unique=True)
     email = StringField(required=True, unique=True)
+    created_at = DateTimeField(default=datetime.utcnow)
