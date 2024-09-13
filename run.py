140a2f134342c44a7eca5c3a4710dd667e3a4191
shubhamsinghsurepass <shubham.singh@surepass.io>
2024-09-13 21:07:39 +0530

    Implemented Upload Pdf API And integrate AI model in that

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import app, api
-from web.api.admin.views import ns
+from web.api.admin.views import ns as admin_ns
+from web.api.upload_pdf.views import ns as pdf_nsd
 
 # Add the namespace to the API
-api.add_namespace(ns)
+api.add_namespace(admin_ns)
+api.add_namespace(pdf_nsd)
 
 if __name__ == '__main__':
     app.run(debug=True)
